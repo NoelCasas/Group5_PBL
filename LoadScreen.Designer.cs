9df00ba6a2a6e0db67f03ff18676e0ead879b30e
MarthaM25 <71932238+MarthaM25@users.noreply.github.com>
2021-05-23 01:37:40 +0800

    ver2.5

--- LoadScreen.Designer.cs
+++ LoadScreen.Designer.cs
@@ -89,7 +89,7 @@ namespace Group5_PBL
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 13);
             this.label2.TabIndex = 3;
-            this.label2.Text = "Ver. 1.5";
+            this.label2.Text = "Ver. 2.5";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // timer1
