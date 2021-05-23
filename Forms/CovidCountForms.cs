d2a814fcbc297917f43e532f77194a850bc6626b
Keziah-isa-duck <76198512+Keziah-isa-duck@users.noreply.github.com>
2021-05-23 16:33:08 +0800

    Feb-March code for comboBox selection

--- Forms/CovidCountForms.cs
+++ Forms/CovidCountForms.cs
@@ -37,12 +37,12 @@ namespace Group5_PBL.Forms
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (comboBox1.SelectedIndex == 0)
+            if (comboBox1.SelectedIndex == 0) // January
             {
                 startDate = new DateTime(2021, 1, 1);
                 endDate = new DateTime(2021, 1, 31);
             }
-            if (comboBox1.SelectedIndex == 1)
+            if (comboBox1.SelectedIndex == 1) // January 1st Week
             {
                 startDate = new DateTime(2021,1,3);
                 endDate = new DateTime(2021,1,9);
@@ -62,12 +62,12 @@ namespace Group5_PBL.Forms
                 startDate = new DateTime(2021, 1, 24);
                 endDate = new DateTime(2021, 1, 30);
             }
-            if (comboBox1.SelectedIndex == 5)
+            if (comboBox1.SelectedIndex == 5) // February
             {
-                startDate = new DateTime(2021, 2, 1);
+                startDate = new DateTime(2021, 2, 1); 
                 endDate = new DateTime(2021, 2, 28);
             }
-            if (comboBox1.SelectedIndex == 6)
+            if (comboBox1.SelectedIndex == 6) // February 1st Week
             {
                 startDate = new DateTime(2021, 2, 7);
                 endDate = new DateTime(2021, 2, 13);
@@ -77,6 +77,43 @@ namespace Group5_PBL.Forms
                 startDate = new DateTime(2021, 2, 14);
                 endDate = new DateTime(2021, 2, 20);
             }
+            if (comboBox1.SelectedIndex == 8)
+            {
+                startDate = new DateTime(2021, 2, 21);
+                endDate = new DateTime(2021, 2, 27);
+            }
+            if (comboBox1.SelectedIndex == 9) 
+            {
+                startDate = new DateTime(2021, 2, 28);
+                endDate = new DateTime(2021, 3, 6);
+            }
+            if (comboBox1.SelectedIndex == 10) // March
+            {
+                startDate = new DateTime(2021, 3, 1);
+                endDate = new DateTime(2021, 3, 31);
+            }
+            if (comboBox1.SelectedIndex == 11) // March 1st Week
+            {
+                startDate = new DateTime(2021, 3, 7);
+                endDate = new DateTime(2021, 3, 13);
+            }
+            if (comboBox1.SelectedIndex == 12)
+            {
+                startDate = new DateTime(2021, 3, 14);
+                endDate = new DateTime(2021, 3, 20);
+            }
+            if (comboBox1.SelectedIndex == 13)
+            {
+                startDate = new DateTime(2021, 3, 21);
+                endDate = new DateTime(2021, 3, 27);
+            }
+            if (comboBox1.SelectedIndex == 14)
+            {
+                startDate = new DateTime(2021, 3, 28);
+                endDate = new DateTime(2021, 4, 3);
+            }
+
+
         }
     }
 }
